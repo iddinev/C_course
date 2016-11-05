f706bb6abd77bfaed89a58ec7ce2d5a182df1cd9
Ivan Dinev <xidinev@gmail.com>
2016-11-06 00:21:04 +0200

    Week 2 [Homework]: Monty hall 'N' doors with 'p' opened generalization.

--- week2/monty_hall_problem.c
+++ week2/monty_hall_problem.c
@@ -1,6 +1,6 @@
 // Write a progam that runs 'n' simulations of the Monty Hall problem
 // and aggregates wheter it is better to switch doors or not.
-// Works in the case of N doors with N-2 being 'opened' by the host.
+// Works in the case of 'N' doors with 'p' being opened by the host.
 // Check wiki for more info on the Monty Hall problem.
 
 
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 
 
-int single_game(char door, char choosen, char keep)
+int single_game(int door, int choosen, int keep, int n_doors, int n_opened)
 {
     if (keep)
     {
@@ -18,23 +18,24 @@ int single_game(char door, char choosen, char keep)
     else
     {
         if (choosen == door) {return 0;}
-        else {return 1;}
+        else if (!(rand() % (n_doors - n_opened - 1))) {return 1;}
+        else {return 0;}
     }
 }
 
 
-// Generalized function for the case of N doors, and the host opening
-// all but 1 (except the players own ofcourse).
-void iters_of_monty(int n_sims, int n_doors)
+// Generalized function for the case of n_doors doors, and the host opening
+// n_opened loosing doors.
+void iters_of_monty(int n_sims, int n_doors, int n_opened)
 {
     float win, lose;
     int i;
     int k_succ = 0;
     int nok_succ = 0;
     int n_keep = 0;
-    char door = 0;
-    char choosen = 0;
-    char keep = 0;
+    int door = 0;
+    int choosen = 0;
+    int keep = 0;
 
     srand(time(NULL));
 
@@ -46,30 +47,32 @@ void iters_of_monty(int n_sims, int n_doors)
 
         if (keep)
         {
-            k_succ += single_game(door, choosen, keep);
+            k_succ += single_game(door, choosen, keep, 0, 0);
             n_keep++;
         }
         else
         {
-            nok_succ += single_game(door, choosen, keep);
+            nok_succ += single_game(door, choosen, keep, n_doors, n_opened);
         }
     }
 
+    printf("running %d sims with %d doors and %d doors being opened\n", n_sims, n_doors, n_opened);
     win = (k_succ * 100.) / n_keep;
     lose = ((n_keep - k_succ) * 100.) / n_keep;
-    printf("when keeping the door - win: %3.3f%c, lose: %3.3f%c\n", win, 37, lose, 37);
+    printf("when keeping the door - win: %3.3f%%, lose: %3.3f%%\n", win, lose);
     win = (nok_succ * 100.) / (n_sims - n_keep);
     lose = ((n_sims - n_keep - nok_succ) * 100.) / (n_sims - n_keep);
-    printf("when switching the door - win: %3.3f%c, lose: %3.3f%c\n", win, 37, lose, 37);
+    printf("when switching the door - win: %3.3f%%, lose: %3.3f%%\n", win, lose);
 }
 
 
 int main()
 {
     int sims = 1e5;
-    int doors = 100;
+    int doors = 10;
+    int opened = 4;
 
-    iters_of_monty(sims, doors);
+    iters_of_monty(sims, doors, opened);
 
     return 0;
 }
