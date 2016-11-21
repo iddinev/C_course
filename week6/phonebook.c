792a5e54400b661d73450ca1f7fe9c3076d99556
Ivan Dinev <xidinev@gmail.com>
2016-11-21 18:49:11 +0200

    Week 6 [Homework]: File i/o initial commit.

---
+++ week6/phonebook.c
@@ -0,0 +1,13 @@
+// Write a program that can store simple phonebook entries in a file.
+// The entries consist of 2 names and a phonenumber. The program
+// should be able to: 1. add 2. search by name 3. delete 4. exit.
+//
+// The program menu should be:
+// Phonebook menu
+// -----------------
+// 1. Add entry
+// 2. Search by name
+// 3. Delete entry
+// 4. Exit
+// ----------------
+// Enter your choice:
