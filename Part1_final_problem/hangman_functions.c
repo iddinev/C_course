c3159872cd53afe812e4b7178eb93759822c52a0
Ivan Dinev <xidinev@gmail.com>
2017-01-22 22:41:53 +0200

    Part 1 homework: Print the word using singly linked list done.

--- Part1_final_problem/hangman_functions.c
+++ Part1_final_problem/hangman_functions.c
@@ -3,17 +3,129 @@
 #include <string.h>
 
 
+
 #define DEFAULT_WORD_LEN 4
-#define RANDOM_SCALE 35000
-/* #define RANDOM_SCALE 1 */
+/* #define RANDOM_SCALE 35000 */
+#define RANDOM_SCALE 1
 
 
+// Constants and structs.
 
 // Data is stored in ascii.
 const char *dict_file = "./dictionary.dat";
 const char *used_words_file = "./guessedwords.dat";
 const char *gallows_file = "./gallows.dat";
 
+struct list_node
+{
+    char node_val;
+    struct list_node *next_node;
+};
+
+// *END* Constants and structs.
+
+
+
+// Function prototypes:
+
+void push(struct list_node **top_node, char letter);
+
+char pop(struct list_node **top_node);
+
+void freeStack(struct list_node **top_node);
+
+// Return an int from a uniform [0;mod] distribution.
+int randomScaled(int mod);
+
+// Zero out a pointer byte by byte.
+void nullifyPointer(char *ptr, int size);
+
+// Function to portably and properly flush stdin.
+// It requries that the last char in the buffer is newline or EOF.
+// There should be atleast 1 char in the buffer when called.
+void flushSTDIN();
+
+// Function is void as to leave the mem alocation and freeing
+// to the caller.
+void invert_str(char *msg);
+
+// Get malloced memory and copy the inverted gallow in it.
+// Can return the current gallow or go and return the next until EOF.
+// if there are no more gallows - free the mem and return NULL.
+void getGallows(char *gallows, int next);
+
+// Pick a random word (not belonging to the used_words file) newline stripped.
+// If no word is picked, a safe option is returned - the first word
+// that was read from the dictionary with ok len and not already used.
+// If there is no safe option - returns NULL.
+// Function looks a bit messy but - code now, refactor later.
+char* pickRandomWord(int minLen);
+
+//'guessed' should have the same length (not counting the '\0') as '*word'.
+void printWord(struct list_node *word, char guessed[]);
+
+struct list_node *WordToList(char *word)
+
+int mainMenu();
+
+// *END* Function prototypes.
+
+
+
+// Function definitions:
+
+void push(struct list_node **top_node, char letter)
+{
+    struct list_node *new_node;
+    new_node = (struct list_node*)malloc(sizeof(struct list_node));
+
+    if (!*top_node)
+    {
+        new_node->node_val = letter;
+        new_node->next_node = NULL;
+    }
+    else
+    {
+        new_node->node_val = letter;
+        new_node->next_node = *top_node;
+    }
+
+    *top_node = new_node;
+}
+
+
+char pop(struct list_node **top_node)
+{
+    char letter;
+    if (*top_node)
+    {
+        letter = (*top_node)->node_val;
+        struct list_node *tmp_node = *top_node;
+        *top_node = (*top_node)->next_node;
+        free(tmp_node);
+    }
+    else
+    {
+        letter = '\0';
+    }
+
+    return letter;
+}
+
+
+void freeStack(struct list_node **top_node)
+{
+    struct list_node *tmp_node;
+
+    while (*top_node)
+    {
+        tmp_node = *top_node;
+        *top_node = tmp_node->next_node;
+        free(tmp_node);
+    }
+
+    *top_node = NULL;
+}
 
 
 int randomScaled(int mod)
@@ -33,9 +145,6 @@ void nullifyPointer(char *ptr, int size)
 }
 
 
-// Function to portably and properly flush stdin.
-// It requries that the last char in the buffer is newline or EOF.
-// There should be atleast 1 char in the buffer when called.
 void flushSTDIN()
 {
     int c;
@@ -43,9 +152,6 @@ void flushSTDIN()
 }
 
 
-// This function is void as to leave the mem alocation and freeing
-// to the caller.
-/* void invert_str(char *msg, int len) */
 void invert_str(char *msg)
 {
     if (!msg) {return;}
@@ -67,9 +173,6 @@ void invert_str(char *msg)
 }
 
 
-// Get malloced memory and copy the inverted gallow in it.
-// Can return the current gallow or go and return the next until EOF.
-// if there are no more gallows - free the mem and return NULL.
 void getGallows(char *gallows, int next)
 {
     FILE *gallows_flp;
@@ -78,6 +181,7 @@ void getGallows(char *gallows, int next)
     int n_bytes = 0;
     char swap;
     char *tmp = (char*)malloc(sizeof(char));
+    gallows = (char*)realloc(gallows, 7*9*sizeof(char));
     gallows[0] = '\0';
 
     gallows_flp = fopen(gallows_file, "r");
@@ -120,11 +224,6 @@ void getGallows(char *gallows, int next)
 }
 
 
-// Pick a random word (not belonging to the used_words file) newline stripped.
-// If no word is picked, a safe option is returned - the first word
-// that was read from the dictionary with ok len and not already used.
-// If there is no safe option - returns NULL.
-// Function looks a bit messy but - code now, refactor later.
 char* pickRandomWord(int minLen)
 {
     char *chosen = (char*)malloc(minLen*sizeof(char));
@@ -138,6 +237,9 @@ char* pickRandomWord(int minLen)
     FILE *dict_flp, *used_flp;
 
     srand(time(NULL));
+
+    used_flp = fopen(used_words_file, "a+");
+    fclose(used_flp);
 
     dict_flp = fopen(dict_file, "r");
     used_flp = fopen(used_words_file, "r");
@@ -215,11 +317,50 @@ char* pickRandomWord(int minLen)
 }
 
 
+void printWord(struct list_node *word, char guessed[])
+{
+    int remaining = 0;
+    int i = 0;
+
+    while (word)
+    {
+        if (guessed[i])
+        {
+            printf("%c ", word->node_val);
+        }
+        else
+        {
+            printf("_ ");
+            remaining++;
+        }
+        i++;
+        word = word->next_node;
+    }
+    printf(", letters remaining: %d\n", remaining);
+}
+
+
+struct list_node *WordToList(char *word)
+{
+    struct list_node *word_list = NULL;
+    int len = strlen(word);
+
+    for (int i=0; i<len; i++)
+    {
+        push((struct list_node **)&word_list, word[len-1-i]);
+    }
+
+    return word_list;
+}
+
+
 int mainMenu()
 {
     int entry;
     FILE *used_flp;
     char *gallows = (char*)malloc(7*9*sizeof(char));
+    struct list_node *word_list = NULL;
+    char array[] = {0,0,0,0,1,0};
     char *word = NULL;
     char *menu = "================\n"
                  "Hangman menu:\n"
@@ -235,18 +376,23 @@ int mainMenu()
     entry = getchar() - 48;
     flushSTDIN();
 
-    used_flp = fopen(used_words_file, "a+");
-    fclose(used_flp);
 
     switch(entry)
     {
         case 1:
-            word = (char *)pickRandomWord(4);
+            word = (char *)pickRandomWord(DEFAULT_WORD_LEN);
             printf("Chosen word is '%s'\n", word);
+            word_list = WordToList(word);
+            printWord((struct list_node *)word_list, array);
             free(word);
             return 0;
             break;
         case 2:
+            for (int i=0; i<=8; i++)
+            {
+                getGallows(gallows, 1);
+                printf("%s", gallows);
+            }
             return 0;
             break;
         case 3:
@@ -258,3 +404,5 @@ int mainMenu()
             return 1;
     }
 }
+
+// *END* Function definitions.
